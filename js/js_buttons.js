03657f665d43a01f1b266af16679d3ddc5e177b2
RobinStone <vohuanrok@mail.ru>
2018-08-03 14:40:10 +0300

    accessebility created for first page modificated css

--- js/js_buttons.js
+++ js/js_buttons.js
@@ -1,3 +1,12 @@
+//---------------   изменение фона у родителя  ---------------------
+$('.services-item a').on('focus', function(){
+   $(this).css('background-color', 'rgba(129, 179, 210, 0.34)');
+});
+
+$('.services-item a').on('blur', function(){
+   $(this).css('background-color', 'white');
+});
+
 //---------------   блокировка дефолной карты ---------------------- 
 $('.map img').css('display', 'none');
 
